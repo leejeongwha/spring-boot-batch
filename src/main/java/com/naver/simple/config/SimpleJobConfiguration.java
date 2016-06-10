c458b079cd3bee04f417c61ef74d1e246adf4f77
leejeongwha <ljk2491@naver.com>
2016-06-11 01:39:38 +0900

    mapJobRepository 추가

--- src/main/java/com/naver/simple/config/SimpleJobConfiguration.java
+++ src/main/java/com/naver/simple/config/SimpleJobConfiguration.java
@@ -4,6 +4,7 @@ import javax.annotation.Resource;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.DefaultBatchConfigurer;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ import com.naver.simple.listener.SimpleJobCompletionNotificationListener;
 
 @Configuration
 @EnableBatchProcessing
-public class SimpleJobConfiguration {
+public class SimpleJobConfiguration extends DefaultBatchConfigurer {
 	private static final String JOB_NAME = "simpleJob";
 
 	@Autowired
@@ -30,4 +31,9 @@ public class SimpleJobConfiguration {
 	public Job simpleJob() {
 		return jobBuilderFactory.get(JOB_NAME).listener(listener).start(simpleStep).build();
 	}
+
+	/*
+	 * @Override protected JobRepository createJobRepository() throws Exception
+	 * { return new MapJobRepositoryFactoryBean().getObject(); }
+	 */
 }
